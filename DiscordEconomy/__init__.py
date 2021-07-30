904bdbfbe76552a51f1e9795383608c1acc3d646
Nohet <70505658+Nohet@users.noreply.github.com>
2021-07-30 09:39:21 +0200

    Update __init__.py

--- DiscordEconomy/__init__.py
+++ DiscordEconomy/__init__.py
@@ -3,7 +3,7 @@ from .economy import *
 __title__ = "DiscordEconomy"
 __summary__ = "Discord.py extension to create economy easily."
 __uri__ = "https://github.com/Nohet/DiscordEconomy"
-__version__ = "1.1"
+__version__ = "1.2"
 __author__ = "Nohet"
 __email__ = "igorczupryniak503@gmail.com"
 __license__ = "MIT License"
