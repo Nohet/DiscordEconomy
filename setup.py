3fecd6c3042d01ecffbddb498f448d7b4c2f3042
Nohet <70505658+Nohet@users.noreply.github.com>
2021-08-26 23:17:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="DiscordEconomy",
-    version="1.2",
+    version="1.3",
     description="Discord.py extension to create economy easily.",
     long_description=long_description,
     long_description_content_type="text/markdown",
